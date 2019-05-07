bbb3edadc592f4dd06a250ccf951d830bc2165c0
huocs霍 <huochangsheng@tospur.com>
2019-05-07 15:17:56 +0800

    watcher相关

--- src/core/observer/traverse.js
+++ src/core/observer/traverse.js
@@ -19,16 +19,23 @@ export function traverse (val: any) {
 function _traverse (val: any, seen: SimpleSet) {
   let i, keys
   const isA = Array.isArray(val)
+  // 如果不是数组、不是对象、不可扩展、或是 vnode 直接 return
+  // Object.isFrozen()方法判断一个对象是否被冻结。
+  // 一个对象是冻结的是指它不可扩展，所有属性都是不可配置的，且所有数据属性（即没有getter或setter组件的访问器的属性）都是不可写的
   if ((!isA && !isObject(val)) || Object.isFrozen(val) || val instanceof VNode) {
     return
   }
+  // 如果有 __ob__ 属性，则表示是一个响应式对象
   if (val.__ob__) {
+    // 获取到 depId 判断 seenObjects 就否 has, 有则直接返回，没有则 add 进去
     const depId = val.__ob__.dep.id
     if (seen.has(depId)) {
       return
     }
     seen.add(depId)
   }
+  // 如果是数组循环数组，若是对象遍历对象
+  // 再调用 _traverse 获取到 depId add进 seenObjects
   if (isA) {
     i = val.length
     while (i--) _traverse(val[i], seen)
