e4b71d98232a720657057271f6af49910f424550
huocs霍 <huochangsheng@tospur.com>
2019-05-08 10:13:40 +0800

    watcher相关

--- src/core/observer/traverse.js
+++ src/core/observer/traverse.js
@@ -4,6 +4,8 @@ import { _Set as Set, isObject } from '../util/index'
 import type { SimpleSet } from '../util/index'
 import VNode from '../vdom/vnode'
 
+// seenObjects 对象做为一个深层依赖收集的临时存储
+// 标识是否经过依赖收集
 const seenObjects = new Set()
 
 /**
@@ -29,19 +31,23 @@ function _traverse (val: any, seen: SimpleSet) {
   if (val.__ob__) {
     // 获取到 depId 判断 seenObjects 就否 has, 有则直接返回，没有则 add 进去
     const depId = val.__ob__.dep.id
+    // 已经经过依赖收集就不再进行依赖收集
     if (seen.has(depId)) {
       return
     }
+    // 没有经过依赖收集再进行依赖收集
     seen.add(depId)
   }
   // 如果是数组循环数组，若是对象遍历对象
   // 再调用 _traverse 获取到 depId add进 seenObjects
   if (isA) {
     i = val.length
+    // 这里调用 val[i] 其实触发了对象的 get 方法
     while (i--) _traverse(val[i], seen)
   } else {
     keys = Object.keys(val)
     i = keys.length
+    // 这里调用 val[i] 其实触发了对象的 get 方法
     while (i--) _traverse(val[keys[i]], seen)
   }
 }
